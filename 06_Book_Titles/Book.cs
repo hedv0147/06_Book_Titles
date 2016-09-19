21eeb907e9646979db54258b6f4f117c9bf7a699
06_booktitles <hedv0147@edu.eal.dk>
2016-09-19 12:24:03 +0200

    a

--- 06_Book_Titles/Book.cs
+++ 06_Book_Titles/Book.cs
@@ -1,41 +1,54 @@
 ﻿using _06_Book_Titles;
 
 namespace _06_Book_Titles
-{  
+{
     public class Book
- 
+
     {
 
-       private string title;
-        public string GetTitle()
+        private string title = "";
+
+        public string Title
         {
-            return this.Title;
-        }
-        public void SetTitle(string v)
-        {
-            string[] words = v.Split(' ');
-            string result="";
-            bool firstWord = true;
-            foreach (string word in words)
+            get
             {
-                if (word == "the"|| word == "a" || word == "an" || word == "and" || word == "in" || word == "of" || word == "to" && firstWord==false)
+                return title;
+            }
+            set
+            {
+                string[] words = value.Split(' ');
+                string result = "";
+                bool firstWord = true;
+                foreach (string word in words)
+                {
+                    if (word == "the" || word == "a" || word == "an" || word == "and" || word == "in" || word == "of" || word == "to" && firstWord == false)
                     {
-                    result = result + word + " ";
+                        result = result + word + " ";
                     }
-                else
+                    else
                     {
-                    string firstLetter = "";
-                    firstLetter = word.Substring(0, 1);
-                    result = result + firstLetter.ToUpper() + word.Substring(1) + " ";
+                        string firstLetter = "";
+                        firstLetter = word.Substring(0, 1);
+                        result = result + firstLetter.ToUpper() + word.Substring(1) + " ";
                     }
-                firstWord = false;
+                    firstWord = false;
                 }
-            this.Title = result;
-            //return result;
+                title = result.Trim();
+                //return result;
             }
             //v = v.Substring(0, 1);
             //v = v.ToUpper();
             //return v.ToUpper()+v.Substring(0, 1) ;
         }
-         
     }
+}
+                
+                //}
+
+
+       
+    //    public void SetTitle(string v)
+    //    {
+            
+         
+    //}
